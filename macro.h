0b65828326284556dfe6200cec024bfc085b7b33
Jacob Malcy <pcx436@yahoo.com>
2020-11-16 21:51:34 -0700

    Adding more macros!

--- macro.h
+++ macro.h
@@ -5,4 +5,10 @@
 #ifndef HTTPPROXY_MACRO_H
 #define HTTPPROXY_MACRO_H
 
+#define MAXLINE     		8192  /* max text line length */
+#define MAXBUF      		8192  /* max I/O buffer size */
+#define LISTENQ     		1024  /* second argument to listen() */
+#define MAX_CACHE_ENTRIES   30
+#define MAX_THREADS         500
+
 #endif //HTTPPROXY_MACRO_H
