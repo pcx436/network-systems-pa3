708204bbda07806d7567d6fc28c60ced948d76da
Jacob Malcy <pcx436@yahoo.com>
2020-11-16 21:52:49 -0700

    switching away from CPP

--- request.c
+++ request.c
@@ -3,58 +3,17 @@
 //
 
 #include <strings.h>
-#include <cstring>
-#include <sstream>
-#include <iostream>
+#include <string.h>
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "request.h"
 
-const std::string &request::getMethod() const {
-	return method;
-}
-
-void request::setMethod(const std::string &method) {
-	request::method = method;
-}
-
-const std::string &request::getRequestPath() const {
-	return requestPath;
-}
-
-void request::setRequestPath(const std::string &requestPath) {
-	request::requestPath = requestPath;
-}
-
-const std::string &request::getAProtocol() const {
-	return protocol;
-}
-
-void request::setAProtocol(const std::string &aProtocol) {
-	protocol = aProtocol;
-}
-
-const std::string &request::getHost() const {
-	return host;
-}
-
-void request::setHost(const std::string &host) {
-	request::host = host;
-}
-
-int request::getPort() const {
-	return port;
-}
-
-void request::setPort(int port) {
-	request::port = port;
-}
-
-request::request() {}
-
-void request::readRequest(int connfd) {
+void readRequest(int connfd, request *req) {
 	int currentMax = MAXBUF;
-	char *recvBuffer = new char [currentMax];
-	buffer = new char [currentMax];
+	char *recvBuffer = malloc(sizeof(char) * currentMax), *buffer = req->originalBuffer;
+	buffer = malloc(sizeof(char) * currentMax);
 
 	bzero(buffer, currentMax);  // fill receiveBuffer with \0
 	bzero(recvBuffer, currentMax);
@@ -62,6 +21,7 @@ void request::readRequest(int connfd) {
 
 	do {
 		// read in some bytes from the user
+		bzero(recvBuffer, MAXBUF);
 		currentReadNum = recv(connfd, recvBuffer, MAXBUF, 0);
 
 		if (currentReadNum > 0) {
@@ -69,30 +29,69 @@ void request::readRequest(int connfd) {
 
 			if (numReceived > MAXBUF) {
 				currentMax *= 2;
-				buffer = (char *) realloc(buffer, currentMax);
+				buffer = (char *) realloc(buffer, sizeof(char) * currentMax);
 
-				if (buffer == nullptr) {
-					std::cerr << "Failed to allocate buffer to meet client demands." << std::endl;
+				if (buffer == NULL) {
+					perror("Failed to allocate buffer to meet client demands.");
 					exit(1);
 				}
 			}
 
 			strcat(buffer, recvBuffer);
 		} else if (currentReadNum < 0) {
-			std::cerr << "Error reading data from user." << std::endl;
+			perror("Error reading data from user.");
 			exit(1);
 		}
-	} while (strstr(buffer, "\r\n\r\n") == nullptr && currentReadNum > 0);  // not done reading
-	delete[] recvBuffer;
+	} while (strstr(buffer, "\r\n\r\n") == NULL && currentReadNum > 0);  // not done reading
+	free(recvBuffer);
 }
 
-void request::parseRequest() {
-	std::string s = std::string(buffer);
-	std::stringstream ss(s);
+void parseRequest(request *req) {
+	char *tmp = NULL, *savePtr = NULL, *finder = NULL;
+	req->postProcessBuffer = (char *)malloc(strlen(req->originalBuffer));
 
-	while(getline())
+	bzero(req->postProcessBuffer, strlen(req->originalBuffer));
+
+	strcpy(req->postProcessBuffer, req->originalBuffer);
+
+	// grab the method stuff
+	req->method = strtok_r(req->postProcessBuffer, " ", &savePtr);
+	req->requestPath = strtok_r(NULL, " ", &savePtr);
+	req->protocol = strtok_r(NULL, "\n", &savePtr);
+	trimSpace(req->protocol);
+
+	tmp = strtok_r(NULL, " ", &savePtr);
+	if (tmp != NULL && strcmp(tmp, "Host:") == 0){  // host specification
+		tmp = strtok_r(NULL, "\n", &savePtr);  // grab the rest of the line
+
+		if (tmp != NULL) {
+			trimSpace(tmp);
+			finder = strchr(tmp, ':');
+
+			if (finder != NULL) {  // port number found
+				req->port = atoi(finder + 1);
+				finder[0] = '\0';
+			}
+
+			req->host = tmp;
+		} else {
+			perror("Error parsing request: invalid host");
+			exit(1);
+		}
+	}
 }
 
-request::~request() {
-	delete[] buffer;
+/**
+ * Removes trailing spaces from a string.
+ * @param str The string to trim space from.
+ */
+void trimSpace(char *s){
+	int end;
+	if (s == NULL)
+		return;
+
+	end = strlen(s) - 1;
+	while (isspace(s[end]))
+		end--;
+	s[end + 1] = '\0';
 }
--- request.h
+++ request.h
@@ -5,10 +5,40 @@
 #ifndef HTTPPROXY_REQUEST_H
 #define HTTPPROXY_REQUEST_H
 
+#include "macro.h"
+#include <semaphore.h>
+#include <sys/time.h>
 
-class request {
+typedef struct request {
+	char *method;  // should always be GET
+	char *requestPath;
+	char *protocol;
+	char *host;
+	char *originalBuffer;
+	char *postProcessBuffer;
+	int port;
+} request;
 
-};
+typedef struct cacheEntry  {
+	char *requestURL;
+	char *response;
+	struct timeval t;
+} cacheEntry;
+
+typedef struct threadParams {
+	cacheEntry *cache;
+	int *connfd;
+	int *cacheSize;
+	int *numThreads;
+	pthread_mutex_t *cacheMutex;
+	pthread_mutex_t *threadMutex;
+} threadParams;
+
+void readRequest(int connfd, request *req);
+
+void parseRequest(request *req);
+
+void trimSpace(char *s);
 
 
 #endif //HTTPPROXY_REQUEST_H
