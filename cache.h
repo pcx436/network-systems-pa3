2dc3d65015c738931c0469538f033ebabf40d234
Jacob Malcy <pcx436@yahoo.com>
2020-11-17 22:42:37 -0700

    Add possible cache function sigs

--- cache.h
+++ cache.h
@@ -18,4 +18,10 @@ struct cache {
 	int cacheTimeout;
 };
 
+cacheEntry *cacheLookup(struct cache *cache);
+
+void deleteCacheEntry(char *requestPath, struct cache *cache);
+
+void clearCache(struct cache *cache);
+
 #endif //HTTPPROXY_CACHE_H
