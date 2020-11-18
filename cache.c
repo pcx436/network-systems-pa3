5a562c22f858bd8f378e521637fff128392abc2d
Jacob Malcy <pcx436@yahoo.com>
2020-11-17 22:53:36 -0700

    Cache lookup function

--- cache.c
+++ cache.c
@@ -3,9 +3,19 @@
 //
 
 #include "cache.h"
+#include <pthread.h>
+#include <string.h>
 
 char * cacheLookup(char *requestPath, struct cache *cache) {
-	
+	int i;
+	pthread_mutex_lock(cache->mutex);
+
+	for(i = 0; i < *cache->size; i++)
+		if (strcmp(requestPath, cache->array[i].requestURL) == 0)
+			return cache->array[i].response;
+
+	pthread_mutex_unlock(cache->mutex);
+	return NULL;
 }
 
 void deleteCacheEntry(char *requestPath, struct cache *cache) {
