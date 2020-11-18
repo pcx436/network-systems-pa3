aa5637a5006c2c8f73434e8616b46084dd0ac1e2
Jacob Malcy <pcx436@yahoo.com>
2020-11-17 22:33:31 -0700

    Creating cache.c/h

---
+++ cache.c
@@ -0,0 +1,5 @@
+//
+// Created by jmalcy on 11/17/20.
+//
+
+#include "cache.h"
---
+++ cache.h
@@ -0,0 +1,8 @@
+//
+// Created by jmalcy on 11/17/20.
+//
+
+#ifndef HTTPPROXY_CACHE_H
+#define HTTPPROXY_CACHE_H
+
+#endif //HTTPPROXY_CACHE_H
