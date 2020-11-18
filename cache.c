20cabc0853b3ca9501f17747cb07aea7f1afd29d
Jacob Malcy <pcx436@yahoo.com>
2020-11-17 22:42:51 -0700

    Implementing potential cache functions

--- cache.c
+++ cache.c
@@ -3,3 +3,15 @@
 //
 
 #include "cache.h"
+
+cacheEntry *cacheLookup(struct cache *cache) {
+
+}
+
+void deleteCacheEntry(char *requestPath, struct cache *cache) {
+
+}
+
+void clearCache(struct cache *cache) {
+
+}
